7b3550a5ad565b1e69bad0d199b1a4e0a652b7a9
sylarjyd <jiangyide@wuhandsj.com>
2023-10-09 16:18:40 +0800

    问题修复

--- src/main/java/com/jyd/service/impl/PortalUserServiceImpl.java
+++ src/main/java/com/jyd/service/impl/PortalUserServiceImpl.java
@@ -8,18 +8,23 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 
+import javax.annotation.Resource;
+
 /**
  * 门户用户 服务实现类
  *
- * @author 
+ * @author
  * @since 2023-07-19
  */
 @Service
 public class PortalUserServiceImpl extends ServiceImpl<PortalUserMapper, PortalUser> implements IPortalUserService {
 
+	@Resource
+	private PortalUserMapper portalUserMapper;
+
 	@Override
 	public IPage<PortalUserVO> selectPortalUserPage(IPage<PortalUserVO> page, PortalUserVO portalUser) {
-		return page.setRecords(baseMapper.selectPortalUserPage(page, portalUser));
+		return page.setRecords(portalUserMapper.selectPortalUserPage(page, portalUser));
 	}
 
 }
