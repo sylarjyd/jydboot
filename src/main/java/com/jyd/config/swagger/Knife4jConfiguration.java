096040b956c169ba6ac41782d183a705db7a0e2b
sylarjyd <jiangyide@wuhandsj.com>
2023-12-18 10:15:47 +0800

    knife4j配置

--- src/main/java/com/jyd/config/swagger/Knife4jConfiguration.java
+++ src/main/java/com/jyd/config/swagger/Knife4jConfiguration.java
@@ -15,18 +15,14 @@ public class Knife4jConfiguration {
 
     @Bean(value = "dockerBean")
     public Docket dockerBean() {
-        //指定使用Swagger2规范
         Docket docket=new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(new ApiInfoBuilder()
-                //描述字段支持Markdown语法
-                .description("# Knife4j RESTful APIs")
+                .description("接口服务")
                 .termsOfServiceUrl("https://doc.xiaominfo.com/")
                 .version("1.0.0")
                 .build())
-                //分组名称
-                .groupName("用户服务")
+                .groupName("接口服务")
                 .select()
-                //这里指定Controller扫描包路径
                 .apis(RequestHandlerSelectors.basePackage("com.jyd.controller"))
                 .paths(PathSelectors.any())
                 .build();
